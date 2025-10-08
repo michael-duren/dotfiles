aefe1451a741aa413895dc5453154f5f5e529539
Michael Duren <michaeld@michaelduren.com>
2025-10-08 12:51:29 -0500

    fix: harpoon & oil

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,40 +1,64 @@
 return {
-
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local harpoon = require("harpoon")
-		harpoon:setup()
-
-		-- Basic keymaps
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():add()
-		end, { desc = "Harpoon add file" })
-		vim.keymap.set("n", "<C-e>", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end, { desc = "Harpoon menu" })
-
-		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<C-S-P>", function()
-			harpoon:list():prev()
-		end, { desc = "Harpoon prev" })
-		vim.keymap.set("n", "<C-S-N>", function()
-			harpoon:list():next()
-		end, { desc = "Harpoon next" })
-
-		-- Jump to specific files (Primeagen uses Ctrl+h/j/k/l but those might conflict)
-		vim.keymap.set("n", "<leader>1", function()
-			harpoon:list():select(1)
-		end, { desc = "Harpoon file 1" })
-		vim.keymap.set("n", "<leader>2", function()
-			harpoon:list():select(2)
-		end, { desc = "Harpoon file 2" })
-		vim.keymap.set("n", "<leader>3", function()
-			harpoon:list():select(3)
-		end, { desc = "Harpoon file 3" })
-		vim.keymap.set("n", "<leader>4", function()
-			harpoon:list():select(4)
-		end, { desc = "Harpoon file 4" })
-	end,
+	keys = {
+		{
+			"<leader>ha",
+			function()
+				require("harpoon"):list():add()
+			end,
+			desc = "Harpoon add file",
+		},
+		{
+			"<leader>hh",
+			function()
+				require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+			end,
+			desc = "Harpoon menu",
+		},
+		{
+			"<leader>hp",
+			function()
+				require("harpoon"):list():prev()
+			end,
+			desc = "Harpoon prev",
+		},
+		{
+			"<leader>hn",
+			function()
+				require("harpoon"):list():next()
+			end,
+			desc = "Harpoon next",
+		},
+		{
+			"<leader>h1",
+			function()
+				require("harpoon"):list():select(1)
+			end,
+			desc = "Harpoon file 1",
+		},
+		{
+			"<leader>h2",
+			function()
+				require("harpoon"):list():select(2)
+			end,
+			desc = "Harpoon file 2",
+		},
+		{
+			"<leader>h3",
+			function()
+				require("harpoon"):list():select(3)
+			end,
+			desc = "Harpoon file 3",
+		},
+		{
+			"<leader>h4",
+			function()
+				require("harpoon"):list():select(4)
+			end,
+			desc = "Harpoon file 4",
+		},
+	},
+	opts = {},
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,57 +1,61 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup({
-			columns = {
-				"icon",
-				-- "permissions",
-				-- "size",
-				-- "mtime",
+	keys = {
+		{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+		{
+			"<C-j>",
+			function()
+				require("oil").toggle_float()
+			end,
+			desc = "Open Oil",
+		},
+	},
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		columns = {
+			"icon",
+			-- "permissions",
+			-- "size",
+			-- "mtime",
+		},
+		keymaps = {
+			["g?"] = "actions.show_help",
+			["<CR>"] = "actions.select",
+			["<C-s>"] = "actions.select_vsplit",
+			["<C-h>"] = "actions.select_split",
+			["<C-t>"] = "actions.select_tab",
+			["<C-p>"] = "actions.preview",
+			["<C-c>"] = "actions.close",
+			["<C-l>"] = "actions.refresh",
+			["-"] = "actions.parent",
+			["_"] = "actions.open_cwd",
+			["`"] = "actions.cd",
+			["~"] = "actions.tcd",
+			["gs"] = "actions.change_sort",
+			["gx"] = "actions.open_external",
+			["g."] = "actions.toggle_hidden",
+			["g\\"] = "actions.toggle_trash",
+		},
+		use_default_keymaps = true,
+		view_options = {
+			show_hidden = false,
+			is_hidden_file = function(name, bufnr)
+				return vim.startswith(name, ".")
+			end,
+			is_always_hidden = function(name, bufnr)
+				return false
+			end,
+		},
+		float = {
+			padding = 2,
+			max_width = 90,
+			max_height = 0,
+			border = "rounded",
+			win_options = {
+				winblend = 0,
 			},
-			keymaps = {
-				["g?"] = "actions.show_help",
-				["<CR>"] = "actions.select",
-				["<C-s>"] = "actions.select_vsplit",
-				["<C-h>"] = "actions.select_split",
-				["<C-t>"] = "actions.select_tab",
-				["<C-p>"] = "actions.preview",
-				["<C-c>"] = "actions.close",
-				["<C-l>"] = "actions.refresh",
-				["-"] = "actions.parent",
-				["_"] = "actions.open_cwd",
-				["`"] = "actions.cd",
-				["~"] = "actions.tcd",
-				["gs"] = "actions.change_sort",
-				["gx"] = "actions.open_external",
-				["g."] = "actions.toggle_hidden",
-				["g\\"] = "actions.toggle_trash",
-			},
-			use_default_keymaps = true,
-			view_options = {
-				show_hidden = false,
-				is_hidden_file = function(name)
-					return vim.startswith(name, ".")
-				end,
-				is_always_hidden = function()
-					return false
-				end,
-			},
-			float = {
-				padding = 2,
-				max_width = 90,
-				max_height = 0,
-				border = "rounded",
-				win_options = {
-					winblend = 0,
-				},
-			},
-		})
-
-		-- Open parent directory in current window
-		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
-		-- Open parent directory in floating window
-		vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { desc = "Open parent directory in float" })
-	end,
+		},
+	},
 }
