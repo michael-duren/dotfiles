acb7041281fea1d8632d25e2238e397fb2d412d8
Michael Duren <michaeld@michaelduren.com>
2025-10-08 14:43:05 -0500

    fix:luasnip&blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,30 +1,57 @@
 return {
-  {
-    "saghen/blink.cmp",
-    version = "*",
-    opts = {
-      keymap = {
-        preset = "default",
-        ["<C-j>"] = { "select_next", "fallback" },
-        ["<C-k>"] = { "select_prev", "fallback" },
-      },
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "mono",
-      },
-      sources = {
-        default = { "lsp", "path", "buffer" },
-      },
-      completion = {
-        menu = {
-          border = "rounded",
-        },
-        documentation = {
-          window = {
-            border = "rounded",
-          },
-        },
-      },
-    },
-  },
+	{
+		"saghen/blink.cmp",
+		version = "*",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+		},
+		opts = {
+			keymap = {
+				preset = "default",
+				["<Up>"] = { "select_prev", "fallback" },
+				["<Down>"] = { "select_next", "fallback" },
+				["<C-k>"] = { "select_prev", "fallback" },
+				["<C-j>"] = { "select_next", "fallback" },
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			},
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+			sources = {
+				default = { "lsp", "path", "luasnip", "buffer" },
+			},
+			snippets = {
+				expand = function(snippet)
+					require("luasnip").lsp_expand(snippet)
+				end,
+				active = function(filter)
+					if filter and filter.direction then
+						return require("luasnip").jumpable(filter.direction)
+					end
+					return require("luasnip").in_snippet()
+				end,
+				jump = function(direction)
+					require("luasnip").jump(direction)
+				end,
+			},
+			completion = {
+				menu = {
+					border = "rounded",
+				},
+				documentation = {
+					window = {
+						border = "rounded",
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			require("blink.cmp").setup(opts)
+			-- Load friendly-snippets
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
 }
