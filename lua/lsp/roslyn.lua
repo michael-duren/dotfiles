4faa77658f10708c35c7dfe8b0d5fde231e0538e
Michael Duren <michaelduren@maximus.com>
2025-10-09 09:01:58 -0700

    feat: update mason

--- lua/lsp/roslyn.lua
+++ lua/lsp/roslyn.lua
@@ -1,1 +1,25 @@
 vim.lsp.config("roslyn", {})
+
+-- Disable formatting capability
+-- NOTE: Come back and figure out how to setup prettier with razor plugin
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Disable formatting capability for rzls
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client and client.name == "rzls" then
+			client.server_capabilities.documentFormattingProvider = false
+		end
+	end,
+})
+
+-- Removes the modified flag from virtual buffers created by rzls for Razor files.
+-- This prevents being prompted to save the buffer when closing it.
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*__virtual*",
+	callback = function()
+		vim.bo.modified = false
+		vim.bo.bufhidden = "wipe"
+		vim.bo.buflisted = false
+	end,
+})
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,19 +1,46 @@
-return {{
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-        require("mason").setup({
-            PATH = "prepend",
-            registries = { -- additional registries required for rosyln
-            "github:mason-org/mason-registry", "github:Crashdummyy/mason-registry"},
-            ensure_installed = {"lua_ls", "gopls", "csharp_ls", "zls", "html", "cssls", "tsserver",
-                                "lua-language-server", "xmlformatter", "csharpier", "prettier", "stylua", "bicep-lsp",
-                                "html-lsp", "eslint-lsp", "typescript-language-server", "json-lsp", "rust-analyzer",
-                                "tailwind-language-server", "roslyn", "rzls"}
-        })
-    end
-}, {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {}
-}}
+return {
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup({
+				PATH = "prepend",
+				registries = {
+					-- additional registries required for rosyln
+					"github:mason-org/mason-registry",
+					"github:Crashdummyy/mason-registry",
+				},
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"csharp_ls",
+					"zls",
+					"html",
+					"cssls",
+					"tsserver",
+					"lua-language-server",
+					"xmlformatter",
+					"csharpier",
+					"prettier",
+					"stylua",
+					"bicep-lsp",
+					"html-lsp",
+					"eslint-lsp",
+					"typescript-language-server",
+					"json-lsp",
+					"rust-analyzer",
+					"tailwind-language-server",
+					"roslyn",
+					"rzls",
+					"markdownlint-cli2",
+					"markdown-toc",
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {},
+	},
+}
