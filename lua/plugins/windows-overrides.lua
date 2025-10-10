23b86840d1641b341578c1060a1f03bfe9d23a07
Michael Duren <michaeld@michaelduren.com>
2025-10-10 13:34:25 -0500

    feat: more windows disabling plugins

---
+++ lua/plugins/windows-overrides.lua
@@ -0,0 +1,13 @@
+local utils = require("helpers.utils")
+
+if not utils.is_windows() then
+	return {}
+end
+
+return {
+	{ "folke/noice.nvim", enabled = false },
+	{ "folke/todo-comments.nvim", enabled = false },
+	{ "zaldih/themery.nvim", enabled = false },
+	{ "windwp/nvim-ts-autotag", enabled = false },
+	{ "github/copilot.vim", enabled = false },
+}
