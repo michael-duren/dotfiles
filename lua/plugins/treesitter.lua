b21fd69b8f2d77def3324a8ba85fc074c7f572d6
Michael Duren <michaeld@michaelduren.com>
2025-10-11 17:58:04 -0500

    fix: treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,41 @@
----@diagnostic disable: missing-fields
 local utils = require("helpers.utils")
 
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	event = { "LazyFile", "VeryLazy" },
-	cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
-	branch = "main",
-	version = false,
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = {
-				"hyprlang",
-				"vim",
-				"lua",
-				"vimdoc",
-				"html",
-				"css",
-				"c_sharp",
-				"razor",
-				"go",
-				"gomod",
-				"gowork",
-				"gosum",
-				"typescript",
-				"javascript",
-				"json",
-				"markdown",
-				"zig",
-			},
-			folds = { enable = true },
-			auto_install = not utils.is_windows(),
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-		})
-	end,
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+			"go",
+			"gomod",
+			"gowork",
+			"gosum",
+			"hyprlang",
+			"css",
+			"c_sharp",
+			"razor",
+			"typescript",
+			"javascript",
+			"json",
+			"zig",
+		},
+		auto_install = not utils.is_windows(),
+		highlight = {
+			enable = true,
+		},
+		indent = {
+			enable = true,
+		},
+	},
 }
