69ba0552d750a306b285d1d60aa7ce89ae9376d3
Michael Duren <michaeld@michaelduren.com>
2025-10-12 07:09:09 -0500

    treesitter with go works but testing is broken

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,129 @@
+return {
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"Issafalcon/neotest-dotnet",
+			{
+				"fredrikaverpil/neotest-golang",
+				version = "*", -- Optional, but recommended; track releases
+				build = function()
+					vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait() -- Optional, but recommended
+				end,
+			},
+			{
+				"nvim-treesitter/nvim-treesitter",
+				branch = "main",
+				build = function()
+					vim.cmd(":TSUpdate go")
+				end,
+			},
+		},
+		lazy = true, -- Load on demand
+		opts = {
+			discovery = {
+				enabled = true,
+			},
+			adapters = {
+				["neotest-golang"] = {
+					dap_go_enabled = true,
+					runner = "gotestsum",
+				},
+				["neotest-dotnet"] = {
+					dap_go_enabled = true,
+				},
+			},
+		},
+		keys = {
+			{ "<leader>t", "", desc = "+test" },
+			{
+				"<leader>tt",
+				function()
+					require("neotest").run.run(vim.fn.expand("%"))
+				end,
+				desc = "Run File",
+			},
+			{
+				"<leader>tT",
+				function()
+					---@diagnostic disable-next-line: undefined-field
+					require("neotest").run.run(vim.uv.cwd())
+				end,
+				desc = "Run All Test Files",
+			},
+			{
+				"<leader>tr",
+				function()
+					require("neotest").run.run()
+				end,
+				desc = "Run Nearest",
+			},
+			{
+				"<leader>td",
+				function()
+					---@diagnostic disable-next-line: missing-fields
+					require("neotest").run.run({ strategy = "dap" })
+				end,
+				desc = "Debug Nearest Test",
+			},
+			{
+				"<leader>tl",
+				function()
+					require("neotest").run.run_last()
+				end,
+				desc = "Run Last",
+			},
+			{
+				"<leader>ts",
+				function()
+					require("neotest").summary.toggle()
+				end,
+				desc = "Toggle Summary",
+			},
+			{
+				"<leader>to",
+				function()
+					require("neotest").output.open({ enter = true, auto_close = true })
+				end,
+				desc = "Show Output",
+			},
+			{
+				"<leader>tO",
+				function()
+					require("neotest").output_panel.toggle()
+				end,
+				desc = "Toggle Output Panel",
+			},
+			{
+				"<leader>tS",
+				function()
+					require("neotest").run.stop()
+				end,
+				desc = "Stop",
+			},
+			{
+				"<leader>tw",
+				function()
+					require("neotest").watch.toggle(vim.fn.expand("%"))
+				end,
+				desc = "Toggle Watch",
+			},
+		},
+		config = function(_, opts)
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+					format = function(diagnostic)
+						local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
+
+			require("neotest").setup(opts)
+		end,
+	},
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 local utils = require("helpers.utils")
 
-return {
+return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	main = "nvim-treesitter.configs",
@@ -30,12 +30,17 @@ return {
 			"json",
 			"zig",
 		},
+		-- Autoinstall languages that are not installed
 		auto_install = not utils.is_windows(),
 		highlight = {
 			enable = true,
 		},
-		indent = {
-			enable = true,
-		},
+		indent = { enable = true },
 	},
+	-- There are additional nvim-treesitter modules that you can use to interact
+	-- with nvim-treesitter. You should go explore a few and see what interests you:
+	--
+	--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+	--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+	--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
