e167e99bc5143d8ce9ec5e5fa8846242a04a8fc8
Michael Duren <michaelduren@maximus.com>
2025-11-14 07:39:30 -0800

    fix: nvim-dbee updates

--- nvim/lua/plugins/nvim-dbee.lua
+++ nvim/lua/plugins/nvim-dbee.lua
@@ -8,7 +8,7 @@ return {
 			-- Install tries to automatically detect the install method.
 			-- if it fails, try calling it with one of these parameters:
 			--    "curl", "wget", "bitsadmin", "go"
-			require("dbee").install()
+			require("dbee").install("go")
 		end,
 		keys = {
 			{ "<leader>D", "<cmd>Dbee<cr>", desc = "Open Dbee" },
@@ -22,21 +22,11 @@ return {
 		end,
 	},
 	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			{
-				"MattiasMTS/cmp-dbee",
-				dependencies = {
-					{ "kndndrj/nvim-dbee" },
-				},
-				ft = "sql", -- optional but good to have
-				opts = {}, -- needed
-			},
-		},
-		opts = {
-			sources = {
-				{ "cmp-dbee" },
-			},
-		},
+		"MattiasMTS/cmp-dbee",
+		dependencies = { "kndndrj/nvim-dbee" },
+		ft = "sql",
+		config = function()
+			require("cmp-dbee").setup({})
+		end,
 	},
 }
