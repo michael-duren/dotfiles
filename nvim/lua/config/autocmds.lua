b55c621c5dcd05bc5f46c16389765ca35a825206
Michael Duren <michaeld@michaelduren.com>
2025-11-26 07:57:56 -0600

    feat:update ts to use 4 space

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,6 @@
 -- set tab size to 2 for json, css, html files
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "json,css,html,typescript,javascript,typescriptreact,javascriptreact,scss,sass,lua,yaml,markdown",
+	pattern = "json,css,html,typescriptreact,javascriptreact,scss,sass,lua,yaml,markdown",
 	callback = function()
 		vim.opt.tabstop = 2
 		vim.opt.softtabstop = 2
