34c3f65e1a4abbdcaf95016029f3f618542b62be
Michael Duren <michaeld@michaelduren.com>
2026-01-19 16:25:41 -0600

    fix:rm other schemas for now

--- nvim/lua/lsp/yamlls.lua
+++ nvim/lua/lsp/yamlls.lua
@@ -9,10 +9,10 @@ vim.api.nvim_create_autocmd("FileType", {
 				yaml = {
 					schemas = {
 						["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.29.0-standalone-strict/all.json"] = "*.yaml",
-						["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-						["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-						["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+						-- ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+						-- ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+						-- ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+						-- ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
 					},
 					validate = true,
 					completion = true,
