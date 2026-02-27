8233063d04e0ee51e36f27d11ee80bbd0a907b88
Michael Duren <michaeld@michaelduren.com>
2026-02-27 09:49:56 -0600

    feat: add env var check for copilot

--- dotfiles/.config/nvim/lua/plugins/schema-store.lua
+++ dotfiles/.config/nvim/lua/plugins/schema-store.lua
@@ -1,5 +1,5 @@
 return {
-	"b0o/SchemaStore.nvim",
-	lazy = true,
-	version = false, -- last release is way too old
+  "b0o/SchemaStore.nvim",
+  lazy = true,
+  version = false, -- last release is way too old
 }
