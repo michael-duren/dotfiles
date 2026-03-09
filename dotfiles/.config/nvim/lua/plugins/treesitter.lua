808441ed7585003b68eda346b46d9bfe9f79f3be
Michael Duren <michaeld@michaelduren.com>
2026-03-09 09:18:46 -0500

    fix:treesitter config for main

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -4,39 +4,47 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.config").setup({
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-				"go",
-				"gomod",
-				"gosum",
-				"gowork",
-				"css",
-				"c_sharp",
-				"typescript",
-				"javascript",
-				"json",
-				"zig",
-				"dockerfile",
-				"yaml",
-				"helm",
-			},
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
+		-- Install parsers
+		require("nvim-treesitter").install({
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+			"go",
+			"gomod",
+			"gosum",
+			"gowork",
+			"css",
+			"c_sharp",
+			"typescript",
+			"javascript",
+			"json",
+			"zig",
+			"dockerfile",
+			"yaml",
+			"helm",
+		})
+
+		-- Enable treesitter features for all installed parsers
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "*",
+			callback = function()
+				local ok, _ = pcall(vim.treesitter.start)
+				if ok then
+					-- Enable syntax highlighting
+					vim.treesitter.start()
+
+					-- Enable indentation
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
 		})
 	end,
 }
