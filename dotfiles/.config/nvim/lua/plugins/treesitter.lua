f4d78da216c12da810077fa9d1baa2e1a3de325c
Michael Duren <michaeld@michaelduren.com>
2026-03-09 09:12:23 -0500

    fix:treesiter rename config pkg

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
+		require("nvim-treesitter.config").setup({
 			ensure_installed = {
 				"bash",
 				"c",
