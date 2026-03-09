1917479361acc3b21f5ec12f951b10e0d3d7f361
Michael Duren <michaeld@michaelduren.com>
2026-03-09 13:45:10 -0500

    feat:add project notes

---
+++ dotfiles/.config/nvim/lua/plugins/project-notes.lua
@@ -0,0 +1,9 @@
+return {
+    "michael-duren/project-notes.nvim",
+    opts = {},
+  keys= {
+    {"<leader>pn", "<cmd>ProjectNotes<cr>", {desc = "Project Notes"} },
+    {"<leader>pa", "<cmd>ProjectNotesNew<cr>", {desc = "New project note"} },
+    {"<leader>pf", "<cmd>ProjectNotesFind<cr>", {desc = "Find project note"} }
+  }
+}
