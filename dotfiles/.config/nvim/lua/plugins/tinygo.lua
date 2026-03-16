52a43aa2177b003dc461169db4b1becec44564ed
Michael Duren <michaeld@michaelduren.com>
2026-03-16 06:24:25 -0500

    fix:revert treesitter

--- dotfiles/.config/nvim/lua/plugins/tinygo.lua
+++ dotfiles/.config/nvim/lua/plugins/tinygo.lua
@@ -3,6 +3,8 @@ return {
 	opts = {
 		".tingygo.json",
 	},
+  lazy = false,
+  priority = 1000,
 	keys = {
 		{ "<leader>tp", "<cmd>TinyGoSetTarget pico<CR>", desc = "Tiny go set pico as target" },
 	},
