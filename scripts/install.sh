c88a470b026e7c12ed8f5e8075f71e955636092a
Michael Duren <michaeld@michaelduren.com>
2026-06-24 10:07:45 -0500

    fix: install scrpt

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-destination="$HOME/.local/bin/"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $script_dir
 
 for f in $(ls "$script_dir"); do
     if [[ $f == "install.sh" ]]; then
         continue
     fi
-    f=$(echo $f | sed 's/\.sh$//')
-    tool="$(pwd)/$f"
+    tool="$script_dir/$f"
     chmod +x tool
+    f=$(echo $f | sed 's/\.sh$//')
+    destination="$HOME/.local/bin/$f"
     echo "copying $tool to $destination"
     cp $tool $destination
 done
