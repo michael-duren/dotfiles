f69d50d8bf581cbeb410a525801a0204bf133cd7
Michael Duren <michaeld@michaelduren.com>
2026-06-30 08:41:42 -0500

    feat: update for linux sys

--- scripts/stmux.sh
+++ scripts/stmux.sh
@@ -10,10 +10,10 @@ if [[ "$WORK" == "true" ]]; then
 
     for session in "${sessions[@]}"; do
         case "$session" in
-            scratch)  path="$HOME/Code" ;;
-            pc)       path="$HOME/Code/cia/digital-web-platform-commander" ;;
-            dotfiles) path="$HOME/Code/dotfiles/dotfiles" ;;
-            notes)    path="$HOME/Code/learning" ;;
+        scratch) path="$HOME/Code" ;;
+        pc) path="$HOME/Code/cia/digital-web-platform-commander" ;;
+        dotfiles) path="$HOME/Code/dotfiles/dotfiles" ;;
+        notes) path="$HOME/Code/learning" ;;
         esac
 
         if tmux has-session -t "$session" 2>/dev/null; then
@@ -26,4 +26,24 @@ if [[ "$WORK" == "true" ]]; then
     done
 
     exec tmux attach -t scratch
+else
+    sessions=(career dotfiles boxes)
+
+    for session in "${sessions[@]}"; do
+        case "$session" in
+        career) path="$HOME/Code/career-strategy" ;;
+        dotfiles) path="$HOME/Code/dotfiles" ;;
+        boxes) path="$HOME/Code/boxes" ;;
+        esac
+
+        if tmux has-session -t "$session" 2>/dev/null; then
+            continue
+        fi
+
+        tmux new-session -d -s "$session" -n "cli" -c "$path"
+        tmux new-window -t "$session" -n "nvim" -c "$path" nvim
+        tmux select-window -t "$session:1"
+    done
+
+    exec tmux attach -t career
 fi
